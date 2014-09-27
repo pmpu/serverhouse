8bd78bb3bbc0b3efc7185f5ed2a7c819534c33c0
George <spamgoga@gmail.com>
2014-09-27 11:44:02 +0400

    menu project added

---
+++ test_tasks/code/2_menu/Menu/Assets/Scripts/NewGame.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewGame: MonoBehaviour {
+ 	void OnMouseEnter(){
+		renderer.material.color = Color.red;
+	}
+	void OnMouseExit(){
+		renderer.material.color = Color.white;
+	}
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			GameObject.Find("txtNewGame").renderer.enabled = true;		
+		}
+		else if (Input.GetKeyDown (KeyCode.Space)){
+			GameObject.Find("txtNewGame").renderer.enabled = false;	
+		}
+	}
+}
---
+++ test_tasks/code/2_menu/Menu/Assets/Scripts/QuitGame.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitGame : MonoBehaviour {
+	void OnMouseEnter(){
+		renderer.material.color = Color.red;
+	}
+
+	void OnMouseExit(){
+		renderer.material.color = Color.white;
+	}
+
+	void OnMouseUpAsButton(){
+		Debug.Log("Game exit");
+		Application.Quit ();
+	}
+
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			GameObject.Find("txtQuit").renderer.enabled = true;		
+		}
+		else if (Input.GetKeyDown (KeyCode.Space)){
+			GameObject.Find("txtQuit").renderer.enabled = false;
+		}
+	}
+}
