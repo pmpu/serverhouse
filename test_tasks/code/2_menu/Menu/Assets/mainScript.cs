0ef1a510827220efae68a1d6b6b6e032ea9f5b98
George <spamgoga@gmail.com>
2014-09-27 23:59:26 +0400

    camera rotation on mouse move

---
+++ test_tasks/code/2_menu/Menu/Assets/mainScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainScript : MonoBehaviour {
+	public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+	public RotationAxes axes = RotationAxes.MouseXAndY;
+	public float sensitivityX = 0F;
+	public float sensitivityY = 0F;
+	
+	public float minimumX = -80F;
+	public float maximumX = 80F;
+	
+	public float minimumY = -10F;
+	public float maximumY = 10F;
+	
+	float rotationY = 0F;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (axes == RotationAxes.MouseXAndY)
+		{
+			float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+			
+			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+			
+			transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+		}
+		else if (axes == RotationAxes.MouseX)
+		{
+			transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+		}
+		else
+		{
+			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+			
+			transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+		}
+	}
+}
