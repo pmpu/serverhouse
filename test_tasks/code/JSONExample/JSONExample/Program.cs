68a11f14a3c35c8eb9a307e831178b8e59bbea89
George <spamgoga@gmail.com>
2014-09-29 00:39:31 +0400

    json example

---
+++ test_tasks/code/JSONExample/JSONExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using SimpleJson;
+
+namespace JSONExample
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+			JsonObject jsonObject = new JsonObject();
+
+
+			JsonArray pointsArray = new JsonArray ();
+
+			for (int i = 0; i < 5; i++) {
+				for (int j = 0; j < 5; j++) {
+					JsonObject point = new JsonObject ();
+					point ["x"] = i;
+					point ["y"] = j;
+					pointsArray.Add (point);
+				}
+			}
+
+			jsonObject ["objectsArray"] = pointsArray;
+			jsonObject["name"] = "foo";
+			jsonObject["num"] = 10;
+			jsonObject["is_vip"] = true;
+			jsonObject["nickname"] = null;
+
+			string jsonString = jsonObject.ToString();
+
+			Console.WriteLine (jsonString);
+
+		}
+	}
+}
