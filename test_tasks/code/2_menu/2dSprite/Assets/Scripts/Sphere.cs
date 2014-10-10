20e97cfd8b8bf30d432c9960555434738ff49255
Nikita Churikov <nikita@chur.ru>
2014-10-10 21:32:28 +0400

    2D Sprite
    Создал сферу, к которой привязан свет такой что при наведении на него
    он появляется и наооборот.
    Добавил к сфере SpriteRenderer.

---
+++ test_tasks/code/2_menu/2dSprite/Assets/Scripts/Sphere.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sphere : MonoBehaviour {
+	void OnMouseEnter(){
+		Behaviour h = (Behaviour)GetComponent("Halo");
+		h.enabled = true;
+	}
+	void OnMouseExit(){
+		Behaviour h = (Behaviour)GetComponent("Halo");
+		h.enabled = false;
+	}
+
+	// Use this for initialization
+	void Start () {
+		Behaviour h = (Behaviour)GetComponent("Halo");
+		h.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
