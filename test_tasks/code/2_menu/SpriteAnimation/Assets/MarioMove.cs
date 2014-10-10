c7f8669b41189d403ec7ed04012c95e8da8a0bc4
Nikita Churikov <nikita@chur.ru>
2014-10-10 23:14:48 +0400

    Animated Sprite
    Сделал жуткое сырую анимацию движения марио. Может двигаться вправо и
    влево.

---
+++ test_tasks/code/2_menu/SpriteAnimation/Assets/MarioMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MarioMove : MonoBehaviour {
+
+	public float Speed = 2;
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		rigidbody2D.velocity = new Vector2 (Input.GetAxis ("Horizontal") * Speed, 0);
+		anim.SetFloat ("hSpeed", Mathf.Abs (rigidbody2D.velocity.x));
+
+//		if (rigidbody2D.velocity.x < 0 && transform.localScale.x >)
+	}
+}
