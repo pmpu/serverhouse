dd3d510d35fb9263984db6827e4c59ad3f449128
Nikita Churikov <nikita@chur.ru>
2014-10-19 23:32:50 +0400

    Выполнены 3 задачи юайника
    Lightened Sphere
    Это шар, к неме прицеплена светящаяся сфера и SpriteRenderer.
    
    Sprite Animation
    Демо того, что мжно сделать спрайтами. Марио ходит вперед и назад + при
    повороте спрайт поворачивается на 180 градуов.
    
    Change texture
    Текстура взята из стандартных в Substance Desiner.
    У тектуры можно менять в данном примере тип камня. Соответственно, если
    потребуется, все остальные атрибуты. К кубу прикреплен скрипт, в нем
    есть метод changeStoneVal. Он принимает в себя float.
    Теперь, у ui slider есть метод on value change. К нему цеплям метод
    changeStoneVal, и теперь, при изменении положеня слайдера,
    соответственно меняется тектура.

---
+++ test_tasks/code/2_menu/Change Texture/Assets/Scripts/Cube.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour {
+	private float stoneVal = 0.0f;
+
+	public void changeStoneVal (float newStoneVal){
+		stoneVal = newStoneVal;
+	}
+
+	void Update () {
+		ProceduralMaterial substance = renderer.sharedMaterial as ProceduralMaterial;
+		if (substance){
+			substance.SetProceduralFloat("Stone_Type", stoneVal);
+			substance.RebuildTextures();
+		}
+	}
+
+	void Start () {
+		
+	}
+}
