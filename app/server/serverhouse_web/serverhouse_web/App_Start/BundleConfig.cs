9ed840b1cb4f64b86a30266c375212665f455a20
George <spamgoga@gmail.com>
2014-11-17 00:27:15 +0400

    bootstrap added

--- app/server/serverhouse_web/serverhouse_web/App_Start/BundleConfig.cs
+++ app/server/serverhouse_web/serverhouse_web/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace serverhouse_web
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+           /* bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
@@ -37,7 +37,12 @@ namespace serverhouse_web
                         "~/Content/themes/base/jquery.ui.tabs.css",
                         "~/Content/themes/base/jquery.ui.datepicker.css",
                         "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+                        "~/Content/themes/base/jquery.ui.theme.css"));*/
+            bundles.Add(new StyleBundle("~/bootstrap/css").Include(
+                   "~/Content/bootstrap/css/themes/sandstone/bootstrap.css"));
+            bundles.Add(new ScriptBundle("~/bootstrap/js").Include(
+                   "~/Content/bootstrap/js/bootstrap.js"));
         }
+           
     }
 }
