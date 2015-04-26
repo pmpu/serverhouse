c9a1633f0b7255abc0eb717c95c1c67bc8de6f8e
annafatkina <annacplus@yandex.ru>
2015-04-27 02:03:05 +0400

    17 cubes

---
+++ art_code_with_textures/8hour/Assets/Render.cs
@@ -0,0 +1,196 @@
+﻿using System.IO;
+using UnityEngine;
+using System.Collections;
+using System.Linq;
+//using System.Windows.UIElement;
+using UnityEditor;
+using UnityEngine.UI;
+
+// ReSharper disable once UnusedMember.Global
+public class Render : MonoBehaviour
+{
+    ProceduralMaterial _substance;
+    public Rect WindowRect0 = new Rect(20, 20, 120, 50);
+    public Rect WindowRect1 = new Rect(20, 100, 120, 50);
+    public Rect SliderRect = new Rect(5, 5, 100, 30);
+    public float SliderValue = 0.0F;
+    public bool doWindow0;
+    public Vector2 Scrolling;
+    public Renderer Rend;
+    public Vector2 ScrollPosition = Vector2.zero;
+    public Material[] Materials;
+    private int _length;
+
+
+    public Render()
+    {
+       
+    }
+
+    void Awake()
+    {
+       // Materials = (ProceduralMaterial[])Resources.FindObjectsOfTypeAll(typeof(ProceduralMaterial));
+        Materials = Resources.FindObjectsOfTypeAll(typeof (ProceduralMaterial)) as Material[];
+
+    }
+
+    void Start()
+    {
+        Debug.Log("I am alive!");
+        Rend = GetComponent<Renderer>();
+        GetComponent<Renderer>().material.shader = Shader.Find("Transparent/Bumped Diffuse");
+        //Materials = (ProceduralMaterial[])Resources.FindObjectsOfTypeAll(typeof(ProceduralMaterial));
+        Debug.Log(Materials.Length + " materials");
+        _length = Materials.Length;
+        doWindow0 = true;
+
+
+        var buttonPrefab = Resources.FindObjectsOfTypeAll<Button>().First();
+        Debug.Log(string.Format("buttons {0} ff", buttonPrefab));
+        var canvas = FindObjectOfType<Canvas>();
+        var materialButtons = Materials.Select(m =>
+        {
+            var result = Instantiate(buttonPrefab);
+            result.transform.SetParent(canvas.transform);
+            result.transform.localPosition = new Vector3(0, 0);
+            result.enabled = true;
+            result.gameObject.SetActive(true);
+            var text = result.targetGraphic.GetComponent<Text>();
+            Debug.Log(result + " is resilt");
+            Debug.Log(text + " is text");
+
+         //   text = m.name; // вытащить текст
+            Debug.Log(text + " is text");
+
+        //    StartCoroutine((IEnumerator )(() => text.));
+         //   result.
+          //  text.text = m.name;
+
+            return result;
+        }).ToList();
+       /*
+        foreach (var m in Materials)
+        {
+            
+            GUI.Button(new Rect(10, 10 + 100*yi, 150, 100), "aaa"+ m.name);
+            Debug.Log(m.name);// очень долго и еще и каждый кадр, вынеси это в старт
+        }
+        */
+
+    }
+
+
+    // unity docs copy 
+
+
+    // end unity docs copy
+
+
+    void OnGUI()
+    {
+        //GUI.HorizontalSlider(new Rect(5, 5, 100, 30), 0, -100, 100);
+        //GUI.Button(new Rect(0, 0, 100, 20), "Top-left");
+        //GUI.Button(new Rect(120, 0, 100, 20), "Top-right");
+        //GUI.Button(new Rect(0, 180, 100, 20), "Bottom-left");
+        //GUI.Button(new Rect(120, 180, 100, 20), "Bottom-right");
+
+        //GUI.Window(0, new Rect (50, 50, 200, 200), doMyWindow, "wind");
+        //windowRect0 = GUI.Window(0, windowRect0, DoMyWindow, "My Window");
+        //windowRect1 = GUI.Window(1, windowRect1, DoMyWindow, "My Window");
+        //GUI.Window (0, new Rect(0,0,300,300), ProceduralPropertiesGUI, "Procedural Properties");
+        
+
+     //   foreach (var m in Materials)
+       // {
+           // GUI.Button(new Rect(10, 10 + 100*yi, 150, 100), "aaa"+ m.name);
+        //    Debug.Log(m.name);// очень долго и еще и каждый кадр, вынеси это в старт
+        //}
+
+
+        if (GUI.Button(new Rect(10, 10, 150, 100), "I am a button"))
+        {
+
+            print("You clicked the button!");
+
+            _substance = GetComponent<Renderer>().material as ProceduralMaterial;
+
+          //  ScrollPosition = GUI.BeginScrollView(new Rect(100, 300, 100, 100), ScrollPosition, new Rect(0, 0, 220, 200));
+
+            if (_substance != null)
+            {
+                _substance.GetProceduralPropertyDescriptions();
+                //Rect windowRect = new Rect (Screen.width - 250, 30, 220, Screen.height - 60);
+                var inputs = _substance.GetProceduralPropertyDescriptions();
+                // try to change properties;
+
+                foreach (var t in inputs)
+                {
+                    Debug.Log(t.type + " " + t.name + " from " + t.minimum + " to " + t.maximum);
+                    if (t.type == ProceduralPropertyType.Float)
+                    {
+
+                        SliderValue = GUI.HorizontalSlider(SliderRect, 0.0F, -100, 100);
+                    }
+                }
+
+                _substance.RebuildTextures();
+
+
+                // попробуй намутить с нормалмапом
+                var bufferTexture = new Texture2D(_substance.mainTexture.width, _substance.mainTexture.height, TextureFormat.RGB24, false);
+                _substance.isReadable = true;
+                _substance.RebuildTexturesImmediately();
+
+                var proceduralTextures = _substance.GetGeneratedTextures();
+
+                var counter = 0;
+                    foreach (var texture in proceduralTextures)
+                    {
+
+                        var proceduralTexture = texture as ProceduralTexture;
+                        if (proceduralTexture != null)
+                        {
+                            bufferTexture.SetPixels32(
+                                proceduralTexture.GetPixels32(0, 0, texture.width, texture.height), 0);
+                            var bytes = bufferTexture.EncodeToPNG();
+                            var fileName = string.Format("{0}{1}_imageppp.png", Application.dataPath, counter);
+                            File.WriteAllBytes(fileName, bytes);
+                        }
+                        else
+                        {
+                            Debug.LogError(string.Format("not a ProceduralTexture! #{0}", counter));
+                        }
+
+                        counter++;
+                    }
+
+            }
+            else
+            {
+                Debug.LogError("There is no Procedural Material", gameObject);
+            }
+          //  GUI.EndScrollView();
+        }
+        // няшность с переключателем - почему в ифе не работает????
+
+        //		doWindow0 = GUI.Toggle(new Rect(10, 10, 100, 20), doWindow0, "Window 0");
+        //		if (doWindow0) {
+        //			windowRect1 = GUI.Window (0, new Rect (110, 10, 200, 60), DoMyWindow, "Basic Window");
+        //		}
+    }
+
+    void DoWindow0(int windowId)
+    {
+        GUI.Button(new Rect(10, 30, 80, 20), "Click Me!");
+        //GUI.DragWindow(new Rect(0, 0, 10000, 10000));
+    }
+
+    void DoMyWindow(int windowId)
+    {
+        if (GUI.Button(new Rect(10, 20, 100, 20), "Hello World"))
+            print("Got a click in window " + windowId);
+
+        GUI.DragWindow(new Rect(0, 0, 10000, 10000));
+    }
+
+}
